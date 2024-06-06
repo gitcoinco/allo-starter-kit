4f899cb00ae89fb788fbc864d8dedf22f0647c8c
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-06-06 11:01:56 +0200

    Cleanup

--- packages/kit/src/rounds/discover.tsx
+++ packages/kit/src/rounds/discover.tsx
@@ -10,6 +10,5 @@ export function DiscoverRounds({
   ...props
 }: GridProps<Round> & { query?: RoundsQuery }) {
   const rounds = useRounds(query!);
-  console.log("rounds", rounds.data, rounds.error);
   return <Grid component={RoundCard} {...rounds} {...props} />;
 }
