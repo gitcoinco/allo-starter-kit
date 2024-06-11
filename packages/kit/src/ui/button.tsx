bd37aac0b67da6195c7ee88acb5a5ac4d819d67c
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-06-11 06:50:10 +0200

    Add Gitcoin colors

--- packages/kit/src/ui/button.tsx
+++ packages/kit/src/ui/button.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 import { LoaderIcon, LucideIcon } from "lucide-react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
@@ -15,8 +15,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         success: "bg-success text-success-foreground hover:bg-success/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        outline: "border border-primary bg-background hover:bg-moss-100/20",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
@@ -24,9 +23,9 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-10 px-4 py-2",
-        xs: "h-8 rounded-md text-xs px-2",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
+        xs: "h-8 rounded-lg text-xs px-2",
+        sm: "h-9 rounded-lg px-3",
+        lg: "h-11 rounded-lg px-8",
         icon: "h-10 w-10",
       },
     },
--- packages/kit/tailwind.config.ts
+++ packages/kit/tailwind.config.ts
@@ -1,9 +1,53 @@
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
-import colors from "tailwindcss/colors";
+import colors, { white } from "tailwindcss/colors";
 
 const defaultGray = "stone";
+
+const moss = {
+  50: "#d3f0ed",
+  100: "#d3f0ed",
+  300: "#307168",
+  600: "#185b52",
+  900: "#00433b",
+};
+const gray = {
+  ...colors.stone,
+  50: "#f7f7f7",
+  100: "#ebebeb",
+  300: "#cacbcb",
+  600: "#979998",
+  900: "#555555",
+};
+const blue = {
+  ...colors.blue,
+  50: "#c1eaff",
+  100: "#4fb8ef",
+  300: "#315871",
+  600: "#082553",
+};
+const green = {
+  ...colors.green,
+  50: "#eafdd4",
+  100: "#eafdd4",
+  300: "#c4f092",
+  600: "#65ac56",
+};
+const yellow = {
+  ...colors.yellow,
+  50: "#fff6ca",
+  100: "#fff6ca",
+  300: "#ffea7d",
+  600: "#ffdd45",
+};
+const red = {
+  ...colors.red,
+  50: "#ffebe4",
+  100: "#ffebe4",
+  300: "#f97a2c",
+  600: "#ae1700",
+};
 const config: Config = {
   darkMode: ["class"],
   content: ["./src/**/*.tsx", "app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
@@ -17,14 +61,22 @@ const config: Config = {
     },
     extend: {
       colors: {
+        gray,
+        blue,
+        yellow,
+        green,
+        red,
+        moss,
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: moss["900"],
+          foreground: white,
+          // DEFAULT: "hsl(var(--primary))",
+          // foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
           DEFAULT: colors[defaultGray][200],
