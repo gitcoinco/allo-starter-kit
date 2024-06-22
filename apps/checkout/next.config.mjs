a3b7c12c61c5f2da1af75561f0dfb32f3f4cd4cf
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-06-22 15:06:06 +0200

    Add images to next.config

--- apps/checkout/next.config.mjs
+++ apps/checkout/next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gateway.pinata.cloud",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
   webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "encoding");
