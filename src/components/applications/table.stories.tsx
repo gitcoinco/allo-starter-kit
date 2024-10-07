01b45e771d23dc85548b83958d47f4bdb9b299b1
Huss Martinez <husse.dev@gmail.com>
2024-10-07 17:43:27 +0700

    chore: migrate from monorepo to normal repo (#43)
    * feat: added shared theme package
    
    * chore: transition from monorepo to normal repo

---
+++ src/components/applications/table.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { ApplicationsTableWithHook } from "./table";
+import { application } from "../../mocks/applications";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Legacy/Applications/Table",
+  component: ApplicationsTableWithHook,
+  parameters: {},
+  argTypes: {},
+  args: {},
+} satisfies Meta<typeof ApplicationsTableWithHook>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    roundId: application.roundId,
+    chainId: application.chainId,
+  },
+};
