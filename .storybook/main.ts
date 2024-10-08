d412f04b48ad11310ea3c9e978fbd82ea2fc7f79
Aditya Anand M C <aditya.anandmc@gmail.com>
2024-10-08 14:16:44 +0530

    remove dead paths

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,11 +10,7 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  // stories: ["../stories/*.stories.tsx", "../stories/**/*.stories.tsx"],
-
   stories: [
-    "../stories/*.stories.tsx",
-    "../stories/**/*.stories.tsx",
     "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
